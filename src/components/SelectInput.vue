16674787832736167498ed9f9376a47f7c91f22f
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-01-14 11:32:13 +0000

    feat: Select Input

---
+++ src/components/SelectInput.vue
@@ -0,0 +1,98 @@
+<template>
+  <div :class="className">
+    <Label v-if="label" :text="label" :htmlFor="name" />
+
+    <div class="input-container">
+      <select
+        :id="name"
+        class="input-style"
+        :value="value"
+        @change="handleChange"
+        :placeholder="placeholder"
+        v-bind="otherProps"
+      >
+        <option value="">{{ placeholder }}</option>
+        <option v-for="option in options" :key="option" :value="option">
+          {{ option }}
+        </option>
+      </select>
+    </div>
+
+    <ErrorMessage v-if="error" :error="error" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SelectInput",
+  props: {
+    className: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    error: {
+      type: Object,
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+    otherProps: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  methods: {
+    handleChange(event) {
+      this.$emit("input", event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-container {
+  position: relative;
+  max-height: 3.5rem;
+  width: 100%;
+}
+
+.input-style {
+  width: 100%;
+  min-width: 177px;
+  padding: 10px 25px;
+  border-radius: 10px;
+  border: 1px solid #ccc;
+  transition: all 0.3s ease;
+}
+
+.input-style:focus {
+  outline: 2px solid #007bff;
+}
+
+.input-style:hover {
+  border-color: #666;
+}
+
+.input-style.error {
+  outline: 2px solid #ff4d4d;
+  background-color: #ffe5e5;
+}
+</style>
--- src/views/RestaurantsView.vue
+++ src/views/RestaurantsView.vue
@@ -1,9 +1,79 @@
 <template>
-  <div>Restaurants</div>
+  <div class="container">
+    <div class="filter-wrapper">
+      <SearchInput
+        :text="searchText"
+        @update:text="updateSearchText"
+        :handleSearch="performSearch"
+        placeholder="Search restaurants..."
+        class="search-input"
+      />
+      <SelectInput
+        label="Cuisine"
+        :options="['Option 1', 'Option 2', 'Option 3']"
+        placeholder="Cuisines"
+        v-model="selectedOption"
+        :error="formErrors.option"
+      />
+
+      <SelectInput
+        label="City"
+        :options="['Option 1', 'Option 2', 'Option 3']"
+        placeholder="Cuisines"
+        v-model="selectedOption"
+        :error="formErrors.option"
+      />
+      <CustomButton text="Search" shape="round" />
+    </div>
+  </div>
 </template>
 <script>
+import CustomButton from "@/components/Button";
+import SearchInput from "@/components/SearchInput";
+import SelectInput from "@/components/SelectInput";
+
 export default {
   name: "RestaurantsView",
+  components: {
+    CustomButton,
+    SearchInput,
+    SelectInput,
+  },
+  data() {
+    return {
+      searchText: "",
+      selectedOption: "",
+      formErrors: {
+        option: null,
+      },
+    };
+  },
+  methods: {
+    updateSearchText(newText) {
+      this.searchText = newText;
+    },
+    performSearch() {
+      console.log("Searching for:", this.searchText);
+    },
+  },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 50px 0;
+
+  .filter-wrapper {
+    display: flex;
+    align-items: center;
+    margin-bottom: 50px;
+    width: 100%;
+
+    .search-input {
+      flex: 1;
+      margin-right: 20px;
+    }
+  }
+}
+</style>
