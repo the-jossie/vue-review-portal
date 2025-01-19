0eab078aaf8f0366c515b3659ae585554801560b
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-01-19 00:04:28 +0000

    feat: input label

---
+++ src/components/Label.vue
@@ -0,0 +1,46 @@
+<template>
+  <label v-if="text" :for="htmlFor" :class="labelClasses">
+    {{ text }}
+    <span v-if="required" class="required-marker">*</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "CustomLabel",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    htmlFor: {
+      type: String,
+      required: true,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    labelClasses() {
+      return ["label-base"];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.label-base {
+  display: block;
+  margin-bottom: 0.25rem;
+  font-size: 0.875rem; /* Adjust as needed */
+  color: #333; /* Adjust as needed */
+}
+
+.required-marker {
+  margin-left: 0.25rem;
+  color: red;
+  font-weight: bold;
+}
+</style>
