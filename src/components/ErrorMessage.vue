b2c517860994d71181073480e0ed779a77684333
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-01-19 00:04:10 +0000

    feat: error message

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div role="alert" class="flex items-center mt-1">
+    <CustomText
+      :value="error.message"
+      variant="p3"
+      class="text-left ml-1 text-red-200"
+    />
+  </div>
+</template>
+
+<script>
+import CustomText from "@/components/Text";
+
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: {
+    CustomText,
+  },
+};
+</script>
+
+<style scoped></style>
