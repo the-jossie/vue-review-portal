f248ac87b57ac6e9dfe3628cbc3167a563f20e00
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-01-25 23:58:23 +0000

    fix: endpoint urls

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,13 +1,13 @@
 import { axiosInstance } from "@/configs";
 
 export const loginApi = async (body) => {
-  const { data } = await axiosInstance.post("/api/auth/login", body);
+  const { data } = await axiosInstance.post("/login", body);
 
   return data;
 };
 
 export const signupApi = async (body) => {
-  const { data } = await axiosInstance.post("/api/auth/register", body);
+  const { data } = await axiosInstance.post("/signup", body);
 
   return data;
 };
